e227294ef305c25304313ca276cd38ed38f83b96
lawrence nwoko <nwokolawrence6@gmail.com>
2020-05-10 21:52:49 +0100

    fixed all

--- src/pages/Navigation/components/Map/Map.js
+++ src/pages/Navigation/components/Map/Map.js
@@ -12,7 +12,7 @@ export const Map = () => {
     return (
         <div className="map" style={{ marginTop:"-15rem"}}>
             <span style={{display:"flex", alignItems: "center", justifyContent:"center"}}><h2 style={{fontSize:"36px"}}>Find Us</h2> <span style={{fontSize:"24px", marginLeft:"5rem"}}>Nigeria Military Archives | Defence Intelligence Agency HeadQuater, Abuja</span></span>
-           <ReactMapGl {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} onViewportChange={viewport => setViewport(viewport) } mapStyle="mapbox://styles/cypherz/ck9wz93ht0xgo1ippknsqny50">
+           <ReactMapGl {...viewport} mapboxApiAccessToken={"pk.eyJ1IjoiY3lwaGVyeiIsImEiOiJjazl3czFnMWUwM3lqM2VxZHEzN2hsbW9yIn0.8bYG15_syBavC8FfezEU6g"} onViewportChange={viewport => setViewport(viewport) } mapStyle="mapbox://styles/cypherz/ck9wz93ht0xgo1ippknsqny50">
                marker here
            </ReactMapGl> 
         </div>
